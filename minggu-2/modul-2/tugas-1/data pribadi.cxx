222e9eddc69b6b5f7e3723396815283223369ab7
rahmadi-riadi <rahmadisdn3@gmqail.com>
2020-10-25 23:51:40 +0800

    tambah tugas modul 2

---
+++ minggu-2/modul-2/tugas-1/data pribadi.cxx
@@ -0,0 +1,19 @@
+//program data pribadi//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"DATA PRIBADI"<<endl;
+	cout<<"Nama			: Rahmadi Priambudi Riadi"<<endl;
+	cout<<"Nim			: 123200155"<<endl;
+	cout<<"Kelas			: F"<<endl;
+	cout<<"Jenis kelamin		: Laki-laki"<<endl;
+	cout<<"Alamat			: Jalan Wirabuana ,Kota Parepare"<<endl;
+	cout<<"e-mail			: 123200155@student.upnyk.ac.id"<<endl;
+	cout<<"No.Hp			: 0811464071"<<endl;
+	
+	
+	return 0;
+}
+
